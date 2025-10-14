d90496232f61b9375673c2c539323e6c3ea114e9
Simonaognyanova05 <simonaognanova05@gmail.com>
2025-10-14 13:38:14 +0300

    Add task 1

---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    internal class Program
+    {
+        static int BinarySearch(int[] arr, int x)
+        {
+            int left = 0;
+            int rigth = arr.Length - 1;
+
+            while(left <= rigth)
+            {
+                int mid = left + (rigth - left) / 2;
+
+                if (arr[mid] == x)
+                {
+                    return mid;
+                }
+                else if (arr[mid] < x)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    left = mid - 1;
+                }
+            }
+
+            return -1;
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 3, 5, 7, 9, 11 };
+            Console.WriteLine(BinarySearch(arr, 5));    
+        }
+    }
+}
