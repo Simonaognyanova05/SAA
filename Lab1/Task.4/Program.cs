357ab57767051e8b936c88bc8188fbf595bd0240
Simonaognyanova05 <simonaognanova05@gmail.com>
2025-10-14 14:17:17 +0300

    Add task 4

---
+++ Lab1/Task.4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task._4
+{
+    internal class Program
+    {
+        static int BinarySearch(int[] arr, int x)
+        {
+            int left = 0;
+            int right = arr.Length - 1;
+            int result = -1;
+
+            while (left <= right)
+            {
+                int mid = left + (right - left) / 2;
+
+                if (arr[mid] <= x)
+                {
+                    result = arr[mid];
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { 2, 4, 6, 8, 10 };
+            Console.WriteLine(BinarySearch(arr, 9));
+        }
+    }
+}
